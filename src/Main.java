2a388234cbcd654a04f2a9f21ee27e19bc802066
Julker Nien Akib <35792784+jakib01@users.noreply.github.com>
2024-01-06 16:09:05 +0600

    :pencil2: Add:-> Bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import sorting.BubbleSorting;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int[] data = {-2, 45, 0, 11, -9};
+        System.out.print("Data to be sorted:-> ");
+        System.out.println(Arrays.toString(data));
+
+        // Bubble sort
+        BubbleSorting obj = new BubbleSorting();
+        obj.bubbleSort(data);
     }
 }
---
+++ src/sorting/BubbleSorting.java
@@ -0,0 +1,22 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSorting {
+    public void bubbleSort(int[] array){
+        int size = array.length;
+
+        for(int i = 0; i < size - 1; i++){
+            for(int j = 0; j < size -1 - i; j++){
+                if(array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.print("After Bubble Sort Apply:-> ");
+        System.out.println(Arrays.toString(array));
+    }
+}
